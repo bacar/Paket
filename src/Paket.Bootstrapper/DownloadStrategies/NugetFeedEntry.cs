6398c91572dd6fc7ad11ffa36270876ccf004b6c
bacar <bacar@acar.org.uk>
2016-08-29 11:48:57 +0100

    Add NugetFeedEntry class with parser & tests

---
+++ src/Paket.Bootstrapper/DownloadStrategies/NugetFeedEntry.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.ServiceModel.Syndication;
+
+namespace Paket.Bootstrapper.Tests.DownloadStrategies
+{
+    class NugetFeedEntry
+    {
+        private readonly string version;
+        private readonly System.Uri contentUrl;
+
+        public string Version { get { return version; } }
+        public System.Uri ContentUrl { get { return contentUrl; } }
+
+        public NugetFeedEntry(System.Xml.XmlReader xmlReader)
+        {
+            // parse the atom feed
+            var feed = SyndicationFeed.Load(xmlReader);
+            var entry = feed.Items.Single();
+            var content = (UrlSyndicationContent)entry.Content;
+            var contentUrl = content.Url;
+            var elementextensions = entry.ElementExtensions.Where(extension => extension.OuterName == "properties").Single().GetObject<System.Xml.Linq.XElement>();
+            var version = elementextensions.Elements().Where(x => x.Name.LocalName == "Version").Single().Value;
+
+            this.version = version;
+            this.contentUrl = contentUrl;
+
+        }
+    }
+}
---
+++ tests/Paket.Bootstrapper.Tests/DownloadStrategies/NugetFeedParserTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using System.ServiceModel.Syndication;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+
+namespace Paket.Bootstrapper.Tests.DownloadStrategies
+{
+    [TestFixture]
+    class NugetFeedParserTests
+    {
+        [Test]
+        public void CanExtractVersionFromFeed()
+        {
+            using (var xmlReader = GetXmlReaderFromFeedResourceStream())
+            {
+                var entry = new NugetFeedEntry(xmlReader);
+                Assert.That(entry.Version, Is.EqualTo("3.16.4"));
+            }
+        }
+
+        [Test]
+        public void CanExtractContentUrlFromFeed()
+        {
+            using (var xmlReader = GetXmlReaderFromFeedResourceStream())
+            {
+                var entry = new NugetFeedEntry(xmlReader);
+
+                Uri expectedUri = new Uri("http://www.nuget.org/api/v2/package/Paket/3.16.4");
+                Assert.That(entry.ContentUrl, Is.EqualTo(expectedUri));
+            }
+        }
+
+        private XmlReader GetXmlReaderFromFeedResourceStream()
+        {
+            var stream = Assembly.GetAssembly(typeof(NugetFeedParserTests))
+                .GetManifestResourceStream("Paket.Bootstrapper.Tests.DownloadStrategies.Nuget.3.16.4.feed.xml");
+            return XmlReader.Create(stream);
+        }
+    }
+}
